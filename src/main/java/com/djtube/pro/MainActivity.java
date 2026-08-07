38f754ce1e738dd5ea1939a9f516ed7ad1ba3de6
jhoanbrazon20-cmyk <jhoanbrazon20@gmail.com>
2026-08-06 19:03:52 -0700

    Update MainActivity.java

--- src/main/java/com/djtube/pro/MainActivity.java
+++ src/main/java/com/djtube/pro/MainActivity.java
@@ -1,18 +1,42 @@
 package com.djtube.pro;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
+        
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        
+        // Habilitar funciones avanzadas
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        
         webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
